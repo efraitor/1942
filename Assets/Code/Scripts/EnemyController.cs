9ae127a709bfa3d409064aedbce32327fe4ef7be
efraitor_ <efraitor.contact@gmail.com>
2023-12-19 10:30:21 +0100

    Update
    Spawn Enemies

--- Assets/Code/Scripts/EnemyController.cs
+++ Assets/Code/Scripts/EnemyController.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    //Referencia al Rigidbody de la nave enemiga
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //La nave se mueve
+        rb.velocity = Vector2.down; //Vector.down = new Vector2(0, -1)
+        //Destruiría el objeto al que está asociado este código pasados 10 segundos
+        Destroy(gameObject, 10f);
     }
 
     // Update is called once per frame
---
+++ Assets/Code/Scripts/EnemySpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    //La jarra de agua. Rellena el contador
+    public float spawnTime;
+    //El contador de tiempo entre spawns
+    private float spawnCounter;
+
+    //Referencia a la nave enemiga
+    public GameObject smallEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Iniciamos el contador
+        spawnCounter = spawnTime;//Rellenamos el contador
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Si el contador de spawn no está vacío
+        if(spawnCounter > 0)
+        {
+            spawnCounter -= Time.deltaTime;
+        }
+
+    }
+
+    public void SpawnEnemy()
+    {
+        //Cogemos un número aleatorio entre esos dos valores
+        float xPosition = Random.Range(-1.0f, 1.0f);
+        Debug.Log(xPosition);
+        //Hacemos aparecer una nave, en la posición del spawner más una posición aleatoria en X, y con la rotación de la nave
+        Instantiate(smallEnemy, transform.position + new Vector3(xPosition, 0f, 0f), smallEnemy.transform.rotation);
+    }
+}
